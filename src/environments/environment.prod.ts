6750d414de3f7c9a2550adf93c72f67180d7c545
Coffee58511867 <marobahlalele910@gmail.com>
2023-03-17 14:29:18 +0200

    Added Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDxms8HYZCk4Rggp2IAXOh51BBcw_6AWxQ",
+    authDomain: "ionic-final-ica.firebaseapp.com",
+    projectId: "ionic-final-ica",
+    storageBucket: "ionic-final-ica.appspot.com",
+    messagingSenderId: "627145086714",
+    appId: "1:627145086714:web:87a7636ecca1f0e4fe5b43",
+    measurementId: "G-ZN9BW4T4XB"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDxms8HYZCk4Rggp2IAXOh51BBcw_6AWxQ",
+    authDomain: "ionic-final-ica.firebaseapp.com",
+    projectId: "ionic-final-ica",
+    storageBucket: "ionic-final-ica.appspot.com",
+    messagingSenderId: "627145086714",
+    appId: "1:627145086714:web:87a7636ecca1f0e4fe5b43",
+    measurementId: "G-ZN9BW4T4XB"
+  },
 };
 
 /*
