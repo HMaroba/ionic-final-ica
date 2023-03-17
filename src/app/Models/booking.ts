c9300dea6076f1e89aed9ca72375a7490fd94521
Coffee58511867 <marobahlalele910@gmail.com>
2023-03-17 17:34:31 +0200

    Fixed Container

--- src/app/Models/booking.ts
+++ src/app/Models/booking.ts
@@ -1,2 +1,8 @@
-export interface Booking {
+export default interface Booking {
+  id? : string;
+  facultyName? : string;
+  date? : Date;
+  purpose? : string;
+  emailAddress? : string;
+  phoneNumbers? : string;
 }
