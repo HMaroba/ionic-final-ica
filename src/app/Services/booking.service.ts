99f5537a3d24c03036814604f2807cabef272427
Coffee58511867 <marobahlalele910@gmail.com>
2023-03-17 17:46:57 +0200

    Fixed Container

---
+++ src/app/Services/booking.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+import Booking from '../Models/booking';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookingService {
+
+  constructor(private ngFirestore: AngularFirestore,
+    private router : Router,) { }
+
+    book(booking: Booking){
+      return this.ngFirestore.collection('Bookings').add(booking);
+    }
+    getBookings(){
+      return this.ngFirestore.collection('Bookings').snapshotChanges();
+    }
+    getBooking(id: any){
+      return this.ngFirestore.collection('Bookings').doc(id).valueChanges()
+    }
+    updateBooking(id: string, booking: Booking){
+      this.ngFirestore.collection('Bookings').doc(id).update(booking)
+      .then(() => {
+        this.router.navigate(['/view-bookings']);
+
+      }).catch(error => console.log(error));
+    }
+    deleteBooking(id : string){
+      this.ngFirestore.doc('Bookings/' +id).delete();
+    }
+  }
