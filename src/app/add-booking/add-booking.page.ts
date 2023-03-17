3d859b484d5e7d610f9b7f6d101af73511a144f7
Coffee58511867 <marobahlalele910@gmail.com>
2023-03-17 18:18:03 +0200

    Added Firebase to app

--- src/app/add-booking/add-booking.page.ts
+++ src/app/add-booking/add-booking.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BookingService } from '../Services/booking.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-booking',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBookingPage implements OnInit {
 
-  constructor() { }
+  bookingform!: FormGroup;
+  isSubmitted = false;
+
+  constructor(public formBuilder: FormBuilder,
+    public bookingService: BookingService,
+    public router: Router,
+    ) { }
 
   ngOnInit() {
+    this.bookingform = this.formBuilder.group({
+      facultyName : ['', Validators.required],
+      purpose: ['', Validators.required],
+      date : ['', Validators.required],
+      emailAddress : ['',[ Validators.required, Validators.email]],
+      phoneNumbers : ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  submitForm(){
+    this.isSubmitted= true;
+    if(this.bookingform.valid){
+      console.log(this.bookingform.value)
+      this.bookingService.book(this.bookingform.value).then((res: any) => {
+        console.log(res)
+        this.bookingform.reset();
+        // this.router.navigate(['/tabs']);
+      })
+        .catch((error: any) => console.log(error));
+    }
+
+  }
+  get errorControl(){
+    return this.bookingform.controls;
   }
 
 }
