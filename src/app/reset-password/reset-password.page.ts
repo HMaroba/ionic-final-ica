b41a57fc2061dd2b9ea7e12da086e7735201d099
Coffee58511867 <marobahlalele910@gmail.com>
2023-03-19 07:31:35 +0200

    Added Reset Password

--- src/app/reset-password/reset-password.page.ts
+++ src/app/reset-password/reset-password.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../api/user.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password.page.scss'],
 })
 export class ResetPasswordPage implements OnInit {
+  rstform! : FormGroup;
+  isSubmitted = false;
 
-  constructor() { }
+  constructor(
+    public userService: UserService,
+    public formBuilder: FormBuilder,
+  ) { }
 
-  ngOnInit() {
+  get errorControl(){
+    return this.rstform.controls;
   }
 
+ngOnInit() {
+  this.rstform = this.formBuilder.group({
+    email : ['',[ Validators.required, Validators.email]],
+  })
+}
+
+  resetForm(email: any){
+    this.isSubmitted = true;
+    if(this.rstform.valid){
+      this.userService.PasswordRecover(email.value);
+    }
+
+  }
 }
