e2fd7939e4754ed6dbb6e49c00fa6a23ab05855c
Coffee58511867 <marobahlalele910@gmail.com>
2023-03-20 21:54:31 +0200

    Fixed User Profile

--- src/app/faculty-profile/faculty-profile.page.ts
+++ src/app/faculty-profile/faculty-profile.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { UserService } from '../api/user.service';
-import UserProfile  from '../Models/UserProfile';
+import UserProfile from '../Models/UserProfile';
 
 @Component({
   selector: 'app-faculty-profile',
@@ -8,18 +10,40 @@ import UserProfile  from '../Models/UserProfile';
   styleUrls: ['./faculty-profile.page.scss'],
 })
 export class FacultyProfilePage implements OnInit {
-
-  users! : UserProfile[];
+  public users!: UserProfile[];
   items: UserProfile[] = [];
-  userDetails: any= [];
+  userDetails: any = [];
 
-  constructor(public userService: UserService) { }
+  constructor(
+    public userService: UserService,
+    public afAuth: AngularFireAuth,
+    public firestore: AngularFirestore
+  ) {}
 
   ngOnInit() {
     const data = localStorage.getItem('profileData');
     this.userDetails = data;
     this.items = Array.of(this.userDetails);
     console.log(this.items);
-  }
 
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.firestore
+          .collection('UsersProfile', (ref) =>
+            ref.where('email', '==', user.email)
+          )
+          .snapshotChanges()
+          .subscribe((data) => {
+            this.users = data.map((t) => {
+              return {
+                id: t.payload.doc.id,
+                ...(t.payload.doc.data() as UserProfile),
+              };
+            });
+          });
+      } else {
+        console.log('No User');
+      }
+    });
+  }
 }
