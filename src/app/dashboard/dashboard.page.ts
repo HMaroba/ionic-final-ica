2d5c3fb9a8106fa6732bba961ffe4ad23dac1f86
Coffee58511867 <marobahlalele910@gmail.com>
2023-03-20 21:34:25 +0200

    Initial commit

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -2,9 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import Booking from '../Models/booking';
 import { BookingService } from '../Services/booking.service';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import UserProfile from '../Models/UserProfile';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,9 +16,11 @@ import UserProfile from '../Models/UserProfile';
 })
 export class DashboardPage implements OnInit {
   public bookings!: Booking[];
+  public bookingList!: Booking[];
   public loadedBookings!: Booking[];
-  currentUser!: any;
-  userData!: UserProfile[];
+  public userInfo! : any;
+  public loadInfo! : any;
+
 
   constructor(
     public formBuilder: FormBuilder,
@@ -39,47 +44,33 @@ export class DashboardPage implements OnInit {
         };
       });
     });
-    const id = localStorage.getItem('userUID');
-    this.firestore.firestore
-      .collection('UsersProfile')
-      .where('uid', '==', id)
-      .get()
-      .then((posts) => {
-        this.userData = posts.docs.map((e) => {
-          return {
-            email: e.data()['email'],
-            phoneNumber: e.data()['phoneNumber'],
-            facultyName: e.data()['facultyName'],
-            status: e.data()['status'],
-            uid: e.data()['uid'],
-            name: e.data()['name'],
-            surname: e.data()['surname'],
-            role: e.data()['role'],
-          };
-        });
-      });
 
-    console.log(this.userData);
+    //get data for only logged in user
     this.afAuth.authState.subscribe((user) => {
       if (user) {
-        const id = user.uid;
         localStorage.setItem('userID', user.uid);
-        console.log(id);
+        this.firestore
+        .collection('Bookings', (ref) => ref.where('userID', '==', user.uid))
+        .valueChanges()
+        .subscribe((data) => {
+          console.log(data);
+          this.userInfo = data;
+          this.loadInfo = data;
+        });
+
+        this.firestore.collection('Bookings' , (ref) => ref.where('userID', '==' , user.uid)).snapshotChanges()
+        .subscribe((data)  => {
+         this.bookingList = data.map((t) => {
+           return {
+             id: t.payload.doc.id,
+             ...(t.payload.doc.data() as Booking),
+           };
+         });
+        });
       } else {
         console.log('No User');
       }
     });
-
-    this.firestore
-      .collection('Bookings')
-      .ref.where('email', '==', id)
-      .onSnapshot((snap) => {
-        snap.forEach((userRef) => {
-          console.log('userRef', userRef.data());
-          this.currentUser = userRef.data();
-          console.log(this.currentUser);
-        });
-      });
   }
   deleteBooking(id: any) {
     console.log(id);
